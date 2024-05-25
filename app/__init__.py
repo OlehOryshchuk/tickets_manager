082cb5f62c4e984e86fbc6ed74c2265c1325c6ad
OlehPro <olehoryshshuk@gmail.com>
2024-05-25 21:28:52 +0200

    Using flask-migrate and flask-login packages and register them in out app

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 from app.config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
+login_manager = LoginManager()
+login_manager.login_view = "auth:login"
 
 
 def init_app():
@@ -13,9 +16,13 @@ def init_app():
     app.config.from_object(Config)
 
     db.init_app(app)
-    migrate.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
 
     with app.app_context():
         db.create_all()
 
     return app
+
+
+from app import models
--- app/config.py
+++ app/config.py
@@ -6,6 +6,7 @@ dotenv.load_dotenv()
 
 class Config:
     ASSETS_ROOT = "app/static"
+    SECRET_KEY = os.environ.get("SECRET_KEY")
 
     # database configurations
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
