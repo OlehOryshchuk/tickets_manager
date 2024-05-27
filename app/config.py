b0cdbbf21bf65dbc02e58e22909e5996dc201f8a
OlehPro <olehoryshshuk@gmail.com>
2024-05-27 21:36:30 +0200

    Create configurations for Debug and Production mode

--- app/config.py
+++ app/config.py
@@ -5,9 +5,31 @@ dotenv.load_dotenv()
 
 
 class Config:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
     ASSETS_ROOT = "app/static"
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "2)%8oosfohOD)@")
 
     # database configurations
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "db.sqlite3")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     PAGINATION_MAX_PER_PAGE = 10
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+
+    SESSION_COOKIE_HTTPONLY = True
+    REMEMBER_COOKIE_HTTPONLY = True
+
+
+class Debug(Config):
+    DEBUG = True
+
+
+config_dict = {
+    "production": ProductionConfig,
+    "debug": Debug
+}
