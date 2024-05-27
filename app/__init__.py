c33dbd57def1c1a0b66e94113a5a7b41d1e980cd
OlehPro <olehoryshshuk@gmail.com>
2024-05-27 21:36:06 +0200

    Make more reusable

--- app/__init__.py
+++ app/__init__.py
@@ -1,32 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 
 from app.config import Config
 from app import models
 
+
 db = SQLAlchemy()
-migrate = Migrate()
 login_manager = LoginManager()
 login_manager.login_view = "auth:login"
-
 csrf_protect = CSRFProtect()
 
 
-def init_app():
-    app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object(Config)
-
-    db.init_app(app)
-    migrate.init_app(app, db)
-    login_manager.init_app(app)
-    csrf_protect.init_app(app)
-
-    with app.app_context():
-        db.create_all()
-
+def register_blueprints(app: Flask):
     from app.routes import (
         auth,
         admin,
@@ -39,5 +26,28 @@ def init_app():
     app.register_blueprint(group.group_bl)
     app.register_blueprint(ticket.ticket_bl)
     app.register_blueprint(user.user_bl)
+
+
+def configure_db(app: Flask):
+    with app.app_context():
+        db.create_all()
+
+    @app.teardown_request
+    def shutdown_session(exception=None):
+        db.session.remove()
+
+
+def register_extensions(app: Flask):
+    login_manager.init_app(app)
+    csrf_protect.init_app(app)
+
+
+def init_app(confi: Config):
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object(config)
+
+    register_extensions(app)
+    register_blueprints(app)
+    configure_db(app)
 
     return app
