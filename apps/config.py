b90a89648ad4c9c419bb2a8aecc7a1df7d61de48
OlehPro <olehoryshshuk@gmail.com>
2024-05-30 04:30:28 +0200

    Create configuration file

--- apps/config.py
+++ apps/config.py
@@ -7,19 +7,19 @@ dotenv.load_dotenv()
 class Config:
     basedir = os.path.abspath(os.path.dirname(__file__))
 
-    ASSETS_ROOT = "app/static"
-    SECRET_KEY = os.environ.get("SECRET_KEY", "2)%8oosfohOD)@")
+    ASSETS_ROOT = "apps/static"
+    SECRET_KEY = os.getenv("SECRET_KEY", "2)%8oosfohOD)@")
 
     # database configurations
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "db.sqlite3")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     PAGINATION_MAX_PER_PAGE = 10
 
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
 
     SESSION_COOKIE_HTTPONLY = True
     REMEMBER_COOKIE_HTTPONLY = True
@@ -29,7 +29,4 @@ class Debug(Config):
     DEBUG = True
 
 
-config_dict = {
-    "production": ProductionConfig,
-    "debug": Debug
-}
+config_dict = {"production": ProductionConfig, "debug": Debug}
