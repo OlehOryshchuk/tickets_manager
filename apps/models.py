7067e7133923fc3d25070aee3acf1c569952142e
OlehPro <olehoryshshuk@gmail.com>
2024-05-30 04:28:04 +0200

    Improve models.py such User, Group, Ticket and their intermediate tables

--- apps/models.py
+++ apps/models.py
@@ -1,18 +1,10 @@
 import enum
 
-from sqlalchemy import (
-    Column,
-    Integer,
-    String,
-    Enum,
-    Text,
-    ForeignKey
-)
-from sqlalchemy.orm import (
-    relationship
-)
+from sqlalchemy import Column, Integer, String, Enum, Text, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
+from flask_login import UserMixin
 
-from . import db
+from apps.extensions import db
 
 
 class Roles(enum.Enum):
@@ -30,18 +22,28 @@ class Status(enum.Enum):
 
 group_tickets = db.Table(
     "group_tickets",
-    db.Column("group_id", Integer, ForeignKey("group.id", ondelete="CASCADE")),
+    db.Column(
+        "group_id",
+        Integer,
+        ForeignKey("group.id", ondelete="CASCADE")
+    ),
     db.Column(
         "ticket_id",
         Integer,
         ForeignKey("ticket.id", ondelete="CASCADE")
-    )
+    ),
 )
 
 user_groups = db.Table(
     "user_groups",
-    db.Column("group_id", Integer, ForeignKey("group.id", ondelete="CASCADE")),
-    db.Column("user_id", Integer, ForeignKey("user.id", ondelete="CASCADE"))
+    db.Column(
+        "group_id",
+        Integer,
+        ForeignKey("group.id", ondelete="CASCADE")),
+    db.Column(
+        "user_id",
+        Integer,
+        ForeignKey("user.id", ondelete="CASCADE")),
 )
 
 
@@ -55,30 +57,30 @@ class Group(db.Model):
         secondary="user_groups",
         backref="groups",
         lazy=True,
-        passive_deletes=True
+        passive_deletes=True,
     )
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(250), nullable=False, unique=True)
-    password = Column(String(100), nullable=False)
+    password = Column(String, nullable=False)
+    active = Column(Boolean, default=True, nullable=True)
+
     role = Column(Enum(Roles), default=Roles.any)
+
+    @property
+    def is_active(self):
+        return self.active
 
 
 class Ticket(db.Model):
     id = Column(Integer, primary_key=True, index=True)
     status = Column(Enum(Status))
-    status = Column(Text, nullable=True)
+    note = Column(Text, nullable=True)
 
     assigned_groups = relationship(
-        "Group",
-        secondary="group_tickets",
-        backref="tickets",
-        lazy=True
+        "Group", secondary="group_tickets", backref="tickets", lazy=True
     )
-    assigned_users = relationship(
-        "User",
-        backref="ticket",
-        lazy=True
-    )
+    assigned_user_id = Column(Integer, ForeignKey("user.id"), nullable=True)
+    assigned_user = relationship("User", backref="tickets", lazy=True)
